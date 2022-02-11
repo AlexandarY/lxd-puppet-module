8a1f7332dfea8eaae06660a04da0093cb81b2935
Alexandar Yovchev <alexyov@protonmail.com>
2022-02-11 23:44:58 +0200

    Rework lxd_storage provider to utilize prefetch
    This change would decrease the amount of API calls required when
    managing multiple lxd_storage resources.

--- lib/puppet/provider/lxd_storage/storage.rb
+++ lib/puppet/provider/lxd_storage/storage.rb
@@ -12,25 +12,22 @@ Puppet::Type.type(:lxd_storage).provide(:storage) do
   # Retrieve all existing storage-pools
   #
   # @return Array[String] names of existing storage pools
-  def get_storage_pools
-    begin # rubocop:disable RedundantBegin
+  def self.get_storage_pools
       response = JSON.parse(lxc(['query', '--wait', '-X', 'GET', '/1.0/storage-pools']))
       response
     rescue JSON::ParserError => err
-      raise Puppet::Error, "Error while retrieving storage-pools - #{err} - #{response}"
-    end
+      raise Puppet::Error, "Error while parsing storage-pools output - #{err} - #{response}"
   end
 
   # Retrieve single storage pool
   #
-  # @param name [String] name of the storage pool
-  def get_storage_pool(name)
-    begin # rubocop:disable RedundantBegin
-      response = JSON.parse(lxc(['query', '--wait', '-X', 'GET', "/1.0/storage-pools/#{name}"]))
-      response
-    rescue JSON::ParserError => err
-      raise Puppet::Error, "Error while retreiving storage-pool #{name} - #{err} - #{response}"
-    end
+  # @param pool_url [String] URL to retrieve pool data. Example: /1.0/storage-pools/pool-name
+  # @return Hash response of API call
+  def self.get_storage_pool(pool_url)
+    response = JSON.parse(lxc(['query', '--wait', '-X', 'GET', pool_url]))
+    response
+  rescue JSON::ParserError => err
+    raise Puppet::Error, "Error while retreiving storage-pool #{name} - #{err} - #{response}"
   end
 
   # Create a new storage-pool
@@ -46,7 +43,7 @@ Puppet::Type.type(:lxd_storage).provide(:storage) do
   # @param pool_name [String] Name of the storage-pool to update
   # @param body [Hash] Values to be updated in storage-pool data
   # @return [nil]
-  def update_existing_storage_pool(pool_name, body)
+  def update_storage_pool(pool_name, body)
     lxc(['query', '--wait', '-X', 'PATCH', '-d', body.to_json, "/1.0/storage-pools/#{pool_name}"])
   end
 
@@ -58,14 +55,15 @@ Puppet::Type.type(:lxd_storage).provide(:storage) do
     lxc(['query', '--wait', '-X', 'DELETE', "/1.0/storage-pools/#{pool_name}"])
   end
 
-  ### Provider methods
+  # Returns a list of system resources (entities) this provider may/can manage.
   def self.instances
-    storage_pools = JSON.parse(lxc(['query', '--wait', '-X', 'GET', '/1.0/storage-pools']))
+    pools = []
+    storage_pools = get_storage_pools
     storage_pools.each do | pool_url |
-      storage_pool = JSON.parse(lxc(['query', '--wait', '-X', 'GET', pool_url]))
+      storage_pool = get_storage_pool(pool_url)
 
       # initializes @property_hash for each storage-pool found
-      new(
+      pools << new(
         :name => storage_pool['name'],
         :ensure => :present,
         :driver => storage_pool['driver'],
@@ -73,14 +71,21 @@ Puppet::Type.type(:lxd_storage).provide(:storage) do
         :config => storage_pool['config']
       )
     end
+    pools
+  end
+
+  # Retrieve all resources
+  def self.prefetch(resources)
+    storage_pools = instances
+    resources.keys.each do | name |
+      if provider = storage_pools.find { |pool| pool.name == name }
+        resources[name].provider = provider
+      end
+    end
   end
 
   # checking if the resource exists
   def exists?
-    # if the entry '/storage-pools/somename' is present within array returned from /storage-pools
-    # then the storage pool somename exists
-    # storage_pools = get_storage_pools
-    # storage_pools.join(',').include? resource[:name]
     @property_hash[:ensure] == :present
   end
 
@@ -108,14 +113,6 @@ Puppet::Type.type(:lxd_storage).provide(:storage) do
 
   # getter method for property config
   def config
-    # storage_info = get_storage_pool(resource[:name])
-    # config_hash = storage_info['config']
-    # Remove volatile.initial_source key from config as
-    # config_hash.delete('volatile.initial_source')
-    # Remove 'source' from config as adjusting it on an
-    # existing storage-pool is dangerous
-    # config_hash.delete('source')
-    # config_hash
     @property_hash[:config]
   end
 
@@ -124,14 +121,11 @@ Puppet::Type.type(:lxd_storage).provide(:storage) do
     call_body = {
       'config' => config_hash,
     }
-    update_existing_storage_pool(resource[:name], call_body)
+    update_storage_pool(resource[:name], call_body)
   end
 
   # getter method for property description
   def description
-    # response = get_storage_pool(resource[:name])
-    # desc = response['description']
-    # desc
     @property_hash[:description]
   end
 
@@ -140,20 +134,16 @@ Puppet::Type.type(:lxd_storage).provide(:storage) do
     call_body = {
       'description' => desc
     }
-    update_existing_storage_pool(resource[:name], call_body)
+    update_storage_pool(resource[:name], call_body)
   end
 
   # getter method for property driver
   def driver
-    # storage_hash = get_storage_pool(resource[:name])
-    # driver = storage_hash['driver']
-    # driver
     @property_hash[:driver]
   end
 
   # setter method for property driver
   def driver=(driver) # rubocop:disable UnusedMethodArgument
-    # TODO: throw some exception as modyfing driver is not supported
     raise NotImplementedError, 'You cannot modify driver of already created storage!'
   end
 end
